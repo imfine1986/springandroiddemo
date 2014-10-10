2024c9b584d20b143fef3d32d01dbdc2d79e4923
zhangshuibo <zhangshuibo@zhangshuibodeMacBook-Pro.local>
2014-10-10 09:41:21 +0800

    spring android demo

---
+++ src/main/java/com/demo/zhangshuibo/springandroiddemo/Greeting.java
@@ -0,0 +1,18 @@
+package com.demo.zhangshuibo.springandroiddemo;
+
+/**
+ * Created by zhangshuibo on 14-10-9.
+ */
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
