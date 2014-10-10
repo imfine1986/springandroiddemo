fe17d23f76b6ba6319180ea183c44167e770055e
zhangshuibo <zhang.shuibo@gmail.com>
2014-10-10 11:44:52 +0800

    android spring two app

--- src/main/java/com/demo/zhangshuibo/springandroiddemo/Note.java
+++ src/main/java/com/demo/zhangshuibo/springandroiddemo/Note.java
@@ -4,4 +4,25 @@ package com.demo.zhangshuibo.springandroiddemo;
  * Created by zhangshuibo on 14-10-10.
  */
 public class Note {
+    private String to;
+    private String from;
+    private String heading;
+    private String body;
+
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getHeading() {
+        return heading;
+    }
+
+    public String getBody() {
+        return body;
+    }
 }
